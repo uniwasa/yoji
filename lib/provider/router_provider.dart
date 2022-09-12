09cfcb2e282c32b4dc0731e3e7f3181cba671c15
uniwasa <dolessup@gmail.com>
2022-09-12 22:04:08 +0900

    ✨熟語ページの雛形作成

--- lib/provider/router_provider.dart
+++ lib/provider/router_provider.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:yoji/page/home/home_page.dart';
+import 'package:yoji/page/idiom/idiom_page.dart';
 import 'package:yoji/page/login/login_page.dart';
 import 'package:yoji/provider/auth_provider.dart';
 
@@ -18,7 +19,17 @@ final routerProvider = Provider(
           name: 'login',
           builder: (context, state) => const LoginPage(),
         ),
+        GoRoute(
+          path: '/idiom',
+          name: 'idiom',
+          builder: (context, state) => const IdiomPage(),
+        ),
       ],
+      redirect: (state) {
+        print('リダイレクト');
+        print(state.subloc);
+        return null;
+      },
       refreshListenable: GoRouterRefreshStream(ref.watch(authProvider.notifier).stream),
     );
   },
